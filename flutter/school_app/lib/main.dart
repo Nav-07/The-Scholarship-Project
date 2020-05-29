756956007ac8fb6924214b223f47aa4fbdbab177
= <navneil2006@gmail.com>
2020-05-30 09:31:43 +1000

    added reame to flutter directory

--- flutter/school_app/lib/main.dart
+++ flutter/school_app/lib/main.dart
@@ -6,7 +6,15 @@ class Application extends StatefulWidget {
 class ApplicationState extends State<Application> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    // Return a Material App from the Build Function.
+    return MaterialApp(
+      // The Title of the App
+      title: 'School Assignment Tracker',
+      // The Home Widget
+      home: null,
+    );
   }
 }
+
+// The main function
+void main() => runApp(Application()); // Run the app
